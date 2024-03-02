bdb518532f92ddebcf58e942ef37e02390dbe8e6
wachuka7 <rachaelwachuka7@gmail.com>
2024-03-02 22:58:07 +0300

    feat:add function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1, 1]
+
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence[:length])
+
+# Example
+print_fibonacci(9)
